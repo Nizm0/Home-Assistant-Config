f2f1be5f875a5d096393ae1d8e6c1a6d094ca89b
Nizm0 <nizmoo@gmail.com>
2019-07-09 15:56:09 +0200

    Removed not used plugins, added some new for wroc

--- www/custom-lovelace/username-element.js
+++
@@ -1,6 +0,0 @@
-class UsernameElement extends HTMLElement {
-  set hass(hass) {this.innerHTML = hass.user.name;}
-  getCardSize() {return 1;}
-  setConfig(config) {}
-}
-customElements.define('username-element', UsernameElement);
