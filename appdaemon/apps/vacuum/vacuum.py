3e10e8c384f1e0ea609dbd677c6a5fab57e9f60b
Nizm0 <nizmoo@gmail.com>
2019-07-17 18:38:04 +0200

    Vacuum appdaemon improvements, new sensors

--- appdaemon/apps/vacuum/vacuum.py
+++ appdaemon/apps/vacuum/vacuum.py
@@ -8,11 +8,13 @@ from globals import HouseModes, PEOPLE
 
 class VacuumActions(Base):
 
+
   def initialize(self):
     # input_time = datetime.time.fromisoformat(self.get_state("input_datetime.vacuum_day_time"))
     # input_time = datetime.time(15, 00)
     # Schedule a daily callback that will call run_daily()
     self.counter = 0
+    self.notify = ["notify/push_to_chrome_nizm0_oneplus3"]
     self.set_vacuum_timer(self, "input_datetime.vacuum_day_time", '', self.get_state("input_datetime.vacuum_day_time"), '')
     self.listen_state(self.change_vacuum_timer, "input_datetime.vacuum_day_time")
     self.listen_state(self.vacuum_state_handle, "vacuum.rockrobo")
@@ -29,22 +31,26 @@ class VacuumActions(Base):
     self.set_vacuum_timer(self, "input_datetime.vacuum_day_time", '', new, '')
 
   def vacuum_state_handle(self, entity, attribute, old, new, kwargs):
-    if new == "error":
-      self.log("Vacuum new state: {new}")
-    elif new == "cleaning":
-      self.log("Vacuum new state: {new}")
-    elif new == "docked":
-      self.log("Vacuum new state: {new}")
-    elif new == "error":
-      self.log("Vacuum new state: {new}")
-    elif new == "idle":
-      self.log("Vacuum new state: {new}")
-    elif new == "pause":
-      self.log("Vacuum new state: {new}")
-    elif new == "returning":
-      self.log("Vacuum new state: {new}")
-    else:
-      self.log("Vacuum new state: {new}")
+    if (old != new):
+      title = "Roborock"
+      if new == "error":
+        self.log("Vacuum new state: {}".format(new))
+      elif new == "cleaning":
+        self.log("Vacuum new state: {}".format(new))
+        message = "Vacuum has started and now is {} is it intended?".format(new)
+        notify_on_start(self, title, self.notify, message)
+      elif new == "docked":
+        self.log("Vacuum new state: {}".format(new))
+      elif new == "error":
+        self.log("Vacuum new state: {}".format(new))
+      elif new == "idle":
+        self.log("Vacuum new state: {}".format(new))
+      elif new == "pause":
+        self.log("Vacuum new state: {}".format(new))
+      elif new == "returning":
+        self.log("Vacuum new state: {}".format(new))
+      else:
+        self.log("Vacuum new state: {}".format(new))
 
   # Our callback function will be called by the scheduler every day
   def run_daily_callback(self, kwargs):
@@ -54,34 +60,18 @@ class VacuumActions(Base):
     home_preset = self.get_state("input_select.home_preset")
     title = "Roborock"
     message = ""
-    notify = ["notify/push_to_chrome_nizm0_oneplus3"]
-    data =  {
-              "vibrate": "200, 100, 200, 100, 200, 100, 200",
-              # "importance": "hight",
-              "renotify": "true",
-              # "timestamp": time,
-              # "priority": 0,
-              "actions": [
-                {
-                  "action": "start_vacuum",
-                  "icon": "/static/icons/favicon-192x192.png",
-                  "title": "Start Vacuum"
-                },
-                {
-                  "action": "cancel",
-                  "title": "Cancel"
-                }],
-              "tag": "home-vacuum-automation"
-            }
-    if ready != "on":
-      message = "Flor is not ready to cleanup? Did you forget about that?"
-      for sender in notify:
-        self.call_service(sender, title = title, message = message, data = data)
-        self.log("Message {} sended to {}".format(title, sender))
-      # self.notify(name="notify.push_to_chrome_nizm0_oneplus3", title = "Hello", message = "Hello World from appDeamon", data=data)
-    elif ocupancy == 'not_home' and home_preset == 'Empty':
-      self.start_vacuum(self)
-      self.log("Vacuum started")
+    # notify = ["notify/push_to_chrome_nizm0_oneplus3"]
+
+    if self.get_state("vacuum.rockrobo") in ["docked", "idle", "pause"]:
+      if ready != "on":
+        message = "Flor is not ready to cleanup? Did you forget about that?"
+        for sender in self.notify:
+          self.call_service(sender, title = title, message = message, data = data)
+          self.log("Message {} sended to {}".format(title, sender))
+        # self.notify(name="notify.push_to_chrome_nizm0_oneplus3", title = "Hello", message = "Hello World from appDeamon", data=data)
+      elif ocupancy == 'not_home' and home_preset == 'Empty':
+        self.start_vacuum(self)
+        self.log("Vacuum started")
 
   def start_vacuum(self):
     self.log("vacuum/start for {}".format("vacuum.rockrobo"))
@@ -97,3 +87,25 @@ class VacuumActions(Base):
     self.log("vacuum/pause for {}".format(seconds))
     self.run_in(self.pause_vacuum, 1)
     self.run_in(self.start_vacuum, seconds)
+
+  def notify_on_start(self, sender, title, message):
+    data =  {
+          "vibrate": "200, 100, 200, 100, 200, 100, 200",
+          # "importance": "hight",
+          "renotify": "true",
+          # "timestamp": time,
+          "priority": "high",
+          "actions": [
+            {
+              "action": "start_vacuum",
+              "icon": "/static/icons/favicon-192x192.png",
+              "title": "Start Vacuum"
+            },
+            {
+              "action": "cancel",
+              "title": "Cancel"
+            }],
+          "tag": "home-vacuum-automation"
+        }
+    for sender in self.notify:
+      self.call_service(sender, title = title, message = message, data = data)
